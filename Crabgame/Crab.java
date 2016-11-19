a31cf042188e1bee2a00e2da9da166963ce7ffe7
lee-sujeong <201602042@cs-cnu.org>
2016-11-19 21:39:34 +0900

    이수정이 한 부분 - 크랩, 웜

--- Crabgame/Crab.java
+++ Crabgame/Crab.java
@@ -8,12 +8,62 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Crab extends Animal
 {
+    private int Speed;
+    public int Score;
+    private int lobster;
     /**
      * Act - do whatever the Crab wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public Crab()
+    {
+        Speed = 100;
+        Score = 0;
+        lobster = 0;
+    }
     public void act() 
     {
         // Add your action code here.
-    }    
+        Move();
+        lookForWorm();
+        addLobster();
+    }
+    public void Move()
+    {
+        
+        int xpos = getX();
+        int ypos = getY();
+        
+        if(Greenfoot.isKeyDown("up")){
+            turn(-5);
+        }
+        if(Greenfoot.isKeyDown("down")){
+            turn(5);
+        }
+        if(Greenfoot.isKeyDown("left")){
+            move(-2);
+        }
+        if(Greenfoot.isKeyDown("right")){
+            move(2);
+        }
+    }
+    
+    public void lookForWorm(){
+        if(isTouching(Worm.class)){
+            removeTouching(Worm.class);
+            Score += 10;
+            lobster += 10;
+            getWorld().showText("Score : "+Score, 50, 10);
+        }
+    }
+
+    public void addLobster(){
+        int x,y;
+        if(lobster % 100 == 0 && lobster > 0){
+            x = Greenfoot.getRandomNumber(500);
+            y = Greenfoot.getRandomNumber(500);
+            getWorld().addObject(new Lobster(), x,y);
+            lobster = 0;
+        }
+    }
 }
--- Crabgame/MyWorld.java
+++ Crabgame/MyWorld.java
@@ -13,9 +13,25 @@ public class MyWorld extends World
      * Constructor for objects of class MyWorld.
      * 
      */
+    
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(500, 500, 1); 
+        super(500, 500, 1);
+        addObject(new Crab(), 300, 300);
+        addObject(new Lobster(), 100, 100);
+        addObject(new Turtle(), 200, 200);
+        prepare();
+    }
+    
+        private void prepare()
+    {
+        int x,y;
+        for (int i = 0 ; i < 5; i++)
+        {
+            x = Greenfoot.getRandomNumber(500);
+            y = Greenfoot.getRandomNumber(500);
+            addObject(new Worm(),x,y);
+        }
     }
 }
